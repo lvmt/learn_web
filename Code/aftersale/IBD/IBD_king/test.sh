b201e40c1fe63378f74498858e742832caa2ba9b
lvmt <13554221497@163.com>
2020-04-21 21:24:08 +0800

    add

--- Code/aftersale/IBD/IBD_king/test.sh
+++
@@ -1,2 +0,0 @@
-plink --vcf /NJPROJ2/DISEASE/WORK/liyujie/AfterSales/05_IBD_plink_king_overlap/IBD_plink/snp.merged.bed.vcf --make-bed --out plink
-/NJPROJ2/DISEASE/WORK/lili/software/king -b plink.bed --related --prefix relation
